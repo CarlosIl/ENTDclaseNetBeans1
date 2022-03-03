40e47abd8a642ed6d2dd23dfbdef624eb7d067ae
DAW <DAW@DESKTOP-78B3935>
2022-03-03 17:17:22 +0100

    Mi primera confirmación desde NetBeans.

---
+++ RemotoPublico1/src/remotopublico1/newClass_1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class_1.java to edit this template
+ */
+
+package remotopublico1;
+
+/**
+ *
+ * @author DAW
+ * Creada: 3 mar 2022 17:11:11
+ * Codificación: UTF-8
+ * Empresa: Ribera
+ */
+public class newClass_1 {
+    
+    int i;
+    
+}
